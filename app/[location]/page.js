95a24daaf2059b1c8945987b2cf7fb86ea642a56
sifatbro786 <solaimanislamsifat@gmail.com>
2024-12-17 01:07:41 +0600

    Location info switcher

--- app/[location]/page.js
+++ app/[location]/page.js
@@ -1,3 +1,8 @@
-export default function LocationPage() {
-    return <div>Location Page</div>;
+import LocationInfo from "@/components/LocationInfo";
+
+export default function LocationPage({
+    params: { location },
+    searchParams: { latitude, longitude },
+}) {
+    return <LocationInfo lat={latitude} lon={longitude} />;
 }
--- components/LocationDetector.jsx
+++ components/LocationDetector.jsx
@@ -28,14 +28,17 @@ export default function LocationDetector() {
     return (
         <div className="flex flex-col justify-center items-center h-screen bg-slate-700 text-white">
             {loading && (
-                <Image
-                    src="/network.gif"
-                    alt="Loading..."
-                    height={500}
-                    width={500}
-                    className="border rounded-md my-4"
-                    unoptimized
-                />
+                <>
+                    <Image
+                        src="/network.gif"
+                        alt="Loading..."
+                        height={500}
+                        width={500}
+                        className="border rounded-md my-4"
+                        unoptimized
+                    />
+                    <p className="text-4xl text-center">Detecting Location...</p>
+                </>
             )}
         </div>
     );

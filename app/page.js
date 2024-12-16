f49c4ecf73975b68c92a49fe0b0437c2c15024d8
sifatbro786 <solaimanislamsifat@gmail.com>
2024-12-16 19:52:25 +0600

    add location detector

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
-export default function HomePage(){
-    return (
-        <div className="text-3xl text-blue-500">
-            HomePage
-        </div>
-    );
+import LocationDetector from "@/components/LocationDetector";
+
+export default function HomePage() {
+    return <LocationDetector />;
 }
